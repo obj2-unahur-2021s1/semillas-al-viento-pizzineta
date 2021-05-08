ae2f5df9f8bb3e01d3a96a98848a6e2b69bd23d1
Juan Marcelo Chaves <63125323+JMChaves94@users.noreply.github.com>
2021-05-08 02:02:21 -0300

    99%

--- src/main/kotlin/ar/edu/unahur/obj2/semillasAlViento/Parcela.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/semillasAlViento/Parcela.kt
@@ -2,44 +2,63 @@ package ar.edu.unahur.obj2.semillasAlViento
 
 class Parcela(val ancho: Int, val largo: Int, val horasSolPorDia: Int) {
   val plantas = mutableListOf<Planta>()
-  var cantidadPlantas = 0
+  var cantidadPlantas = 0 // (des)acoplamiento EN VEZ DE VARIABLE DEBE SER UNA FUNCION SIZE DE LA LISTA PLANTAS
+
 
   fun superficie() = ancho * largo
   fun cantidadMaximaPlantas() =
+    /*redundancia mínima, utiliza la misma fórmula que emplea el método superficie y no lo reutiliza*/
     if (ancho > largo) ancho * largo / 5 else ancho * largo / 3 + largo
 
-  fun plantar(planta: Planta) {
+  fun plantar(planta: Planta) { //fallas de cohesión ya que realiza mas de una acción al mismo tiempo
     if (cantidadPlantas == this.cantidadMaximaPlantas()) {
-      println("Ya no hay lugar en esta parcela")
-    } else if (horasSolPorDia > planta.horasDeSolQueTolera() + 2) {
-      println("No se puede plantar esto acá, se va a quemar")
+      println("Ya no hay lugar en esta parcela") //robustez, debería devolver exception
+    } else if (horasSolPorDia > planta.horasDeSolQueTolera() + 2) { //Simplicidad-KISS
+                                                        //debería ser un metodo aparte el saber si tiene complicaciones
+      println("No se puede plantar esto acá, se va a quemar") //robustez, debería devolver exception
     } else {
       plantas.add(planta)
-      cantidadPlantas += 1
+      cantidadPlantas += 1 //consecuencia del (des)acoplamiento en la linea 5
     }
   }
 }
 
 class Agricultora(val parcelas: MutableList<Parcela>) {
+  /*Fallo en Simplicidad-YAGNI
+  * La consigna nos indica que la tierra no puede ser comprada ni vendida, programaron "por las dudas"*/
   var ahorrosEnPesos = 20000
 
   // Suponemos que una parcela vale 5000 pesos
   fun comprarParcela(parcela: Parcela) {
+    /*Fallo en Simplicidad-YAGNI
+  * La consigna nos indica que la tierra no puede ser comprada ni vendida, programaron "por las dudas"*/
     if (ahorrosEnPesos >= 5000) {
       parcelas.add(parcela)
       ahorrosEnPesos -= 5000
     }
   }
 
+
   fun parcelasSemilleras() =
+    /*Fallos en mutaciones controladas
+ * El método realiza 2 recorridos en 2 listas distintas y genera mutaciones
+ * Debería aplicarse abstraccion para mayor legibilidad del código*/
     parcelas.filter {
       parcela -> parcela.plantas.all {
-        planta -> planta.daSemillas()
+      planta -> planta.daSemillas()
       }
     }
 
+
+
+
   fun plantarEstrategicamente(planta: Planta) {
-    val laElegida = parcelas.maxBy { it.cantidadMaximaPlantas() - it.cantidadPlantas }!!
-    laElegida.plantas.add(planta)
+    /*cohesión ya que realiza 2 acciones al mismo tiempo*/
+    val laElegida = parcelas.maxBy { it.cantidadMaximaPlantas() - it.cantidadPlantas }!! /*simplicidad-KISS deberia hacer un metodo
+    aparte que devuelva la parcela con mayor espacio disponible*/
+    laElegida.plantas.add(planta) /*Redundancia mínima, repite el método plantar y no lo reutiliza*/
+
+
   }
+
 }
--- src/main/kotlin/ar/edu/unahur/obj2/semillasAlViento/Planta.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/semillasAlViento/Planta.kt
@@ -1,6 +1,6 @@
 package ar.edu.unahur.obj2.semillasAlViento
 
-abstract class Planta(val anioObtencionSemilla: Int, var altura: Float) {
+abstract class Planta(val anioObtencionSemilla: Int, var altura: Float) {   //mutación controlada ya que la altura no se deberia poder cambiar
   fun esFuerte() = this.horasDeSolQueTolera() > 10
 
   fun parcelaTieneComplicaciones(parcela: Parcela) =
@@ -16,24 +16,28 @@ class Menta(anioObtencionSemilla: Int, altura: Float) : Planta(anioObtencionSemi
 }
 
 class Soja(anioObtencionSemilla: Int, altura: Float, val esTransgenica: Boolean) : Planta(anioObtencionSemilla, altura) {
+  /* (DES)ACOPLAMIENTO, el error de esTransgenica genera la innecesaria creacion de una constante, arrastrando problemas a futuro*/
   override fun horasDeSolQueTolera(): Int  {
     // ¡Magia de Kotlin! El `when` es como un `if` pero más poderoso:
     // evalúa cada línea en orden y devuelve lo que está después de la flecha.
-    val horasBase = when {
+    val horasBase = when { // simplicidad-KISS, variable innecesaria ya que el return de la linea 28 devolveria directamente
+                           // las horas de sol toleradas segun la altura
       altura < 0.5  -> 6
       altura < 1    -> 7
       else          -> 9
     }
 
     return if (esTransgenica) horasBase * 2 else horasBase
+    //consecuencia de (des)acoplamiento ademas de falta de cohesión ya que resuelve 2 problemas al mismo tiempo
   }
 
 
   override fun daSemillas(): Boolean  {
-    if (this.esTransgenica) {
+    if (this.esTransgenica) { //consecuencia de (des)acoplamiento del constructor de la linea 18
       return false
     }
 
     return this.esFuerte() || (this.anioObtencionSemilla > 2007 && this.altura > 1)
   }
 }
+
